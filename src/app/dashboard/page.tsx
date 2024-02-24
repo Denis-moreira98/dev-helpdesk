fc8e0501eeaef0ce8abac119c865428570836c9b
Denis Moreira <72985107+Denis-moreira98@users.noreply.github.com>
2024-02-23 23:22:40 -0300

    feat: privando a page dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,15 @@
 import { Container } from "@/components/container";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
+import { redirect } from "next/navigation";
 
-export default function Dashboard() {
+export default async function Dashboard() {
+   const session = await getServerSession(authOptions);
+
+   if (!session || !session.user) {
+      redirect("/");
+   }
+
    return (
       <Container>
          <div>
