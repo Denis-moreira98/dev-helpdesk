c6c8c2c1401d8d27b61f702ad973f0db4006ec62
Denis Moreira <72985107+Denis-moreira98@users.noreply.github.com>
2024-02-25 20:05:49 -0300

    feat: deletando cliente

--- src/app/dashboard/customer/components/card/index.tsx
+++ src/app/dashboard/customer/components/card/index.tsx
@@ -1,6 +1,21 @@
+"use client";
+
 import { CustomerProps } from "@/utils/customer.type";
+import { api } from "@/lib/api";
 
 export function CardCustomer({ customer }: { customer: CustomerProps }) {
+   async function handleDeleteCustomer() {
+      try {
+         await api.delete("/api/customer", {
+            params: {
+               id: customer.id,
+            },
+         });
+      } catch (err) {
+         console.log(err);
+      }
+   }
+
    return (
       <article className="flex flex-col bg-gray-100 border-2 p-2 rounded-lg gap-2 hover:scale-105 duration-300">
          <h2>
@@ -12,7 +27,10 @@ export function CardCustomer({ customer }: { customer: CustomerProps }) {
          <p>
             <a className="font-bold">Telefone:</a> {customer.phone}
          </p>
-         <button className="bg-red-500 px-4 rounded text-white mt-2 self-start">
+         <button
+            onClick={handleDeleteCustomer}
+            className="bg-red-500 px-4 rounded text-white mt-2 self-start"
+         >
             Deletar
          </button>
       </article>
