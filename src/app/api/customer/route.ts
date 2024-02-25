ba33f6ea7675b8a6195250e77521495526ed2299
Denis Moreira <72985107+Denis-moreira98@users.noreply.github.com>
2024-02-25 20:05:38 -0300

    feat: rota para deletar clientes

--- src/app/api/customer/route.ts
+++ src/app/api/customer/route.ts
@@ -3,6 +3,41 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
 import prismaClient from "@/lib/prisma";
 
+//Rota para deletar clientes
+export async function DELETE(request: Request) {
+   const session = await getServerSession(authOptions);
+
+   if (!session || !session.user) {
+      return NextResponse.json({ error: "Not authorized" }, { status: 401 });
+   }
+
+   const { searchParams } = new URL(request.url);
+   const userId = searchParams.get("id");
+
+   if (!userId) {
+      return NextResponse.json(
+         { error: "Failded delete customer" },
+         { status: 400 }
+      );
+   }
+
+   try {
+      await prismaClient.customer.delete({
+         where: {
+            id: userId as string,
+         },
+      });
+      return NextResponse.json({ message: "Cliente deletado com sucesso!" });
+   } catch (err) {
+      console.log(err);
+      return NextResponse.json(
+         { error: "Failded delete customer" },
+         { status: 400 }
+      );
+   }
+}
+
+//Rota para cadastrar clientes
 export async function POST(request: Request) {
    const session = await getServerSession(authOptions);
 
