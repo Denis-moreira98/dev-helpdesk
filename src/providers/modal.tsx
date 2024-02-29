81b41128bb7054d6217cdb07646ddfabb32bc9cf
Denis Moreira <72985107+Denis-moreira98@users.noreply.github.com>
2024-02-29 18:17:53 -0300

    feat: criando state para ticket

--- src/providers/modal.tsx
+++ src/providers/modal.tsx
@@ -8,19 +8,26 @@ import { ModalTicket } from "@/components/modal";
 interface ModalContextData {
    visible: boolean;
    handleModalVisible: () => void;
+   ticket: TicketInfo | undefined;
+}
+
+interface TicketInfo {
+   ticket: TicketsProps;
+   custumer: CustomerProps | null;
 }
 
 export const modalContext = createContext({} as ModalContextData);
 
 export const ModalProvider = ({ children }: { children: ReactNode }) => {
    const [visible, setVisible] = useState(false);
+   const [ticket, setTicket] = useState<TicketInfo>();
 
    function handleModalVisible() {
       setVisible(!visible);
    }
 
    return (
-      <modalContext.Provider value={{ visible, handleModalVisible }}>
+      <modalContext.Provider value={{ visible, handleModalVisible, ticket }}>
          {visible && <ModalTicket />}
          {children}
       </modalContext.Provider>
